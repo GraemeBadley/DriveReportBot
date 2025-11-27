28d887578e8c4619f941d5044ec9a97734df47d8
GraemeBadley <gbadley1@gmail.com>
2025-11-27 18:08:16 -0500

    added get_todays_games function

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import requests
+from datetime import date
+    
+def get_todays_games():
+    today = date.today()
+    strdate = f"{today.year}{today.month:02}{today.day:02}"
+    url = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard?dates=" + strdate
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        games = []
+        data = response.json()
+        for event in data["events"]:
+            games.append(event["id"])
+            print(f"{event["shortName"]}, id: {event["id"]}")
+        return games
+    else:
+        raise Exception(f"{response.text}")
+
+    
 def main():
-    print("Hello from drivereportbot!")
-
-
+    games = get_todays_games()
+    print(games)
+    
 if __name__ == "__main__":
     main()
